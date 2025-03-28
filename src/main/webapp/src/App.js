64164ded674cbead1aedf73e242fc4ec9413edd9
IrynaKosiak7 <ira,oleonock2013@gmail.com>
2025-03-28 15:16:02 +0100

    Erstellung frontend Applikation (React)

---
+++ src/main/webapp/src/App.js
@@ -0,0 +1,22 @@
+import logo from './logo.svg';
+import './App.css';
+import { useState } from 'react';
+
+function App() {
+
+  const [value, setValue] = useState(0);
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>Hello universe! <span>{value}</span></p>
+        <div>
+          <button onClick={() => setValue(value + 1)}>+</button>
+          <button onClick={() => setValue(value - 1)}>-</button>
+        </div>
+      </header>
+    </div>
+  );
+}
+
+export default App;
